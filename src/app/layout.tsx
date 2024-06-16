64a37e51b2237d4df47a1efbe2570b70085043d3
Tokom Nyori <tokom.nyori@gmail.com>
2024-06-17 01:10:13 +0530

    geekpie-ai 20

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,10 @@ const dmsans = DM_Sans({
 
 export const metadata: Metadata = {
   title: "GeekPie AI",
-  description: "AI-Powered Chatbots",
+  description: "AI-Powered Chatbots: Disrupt and Capture the Market.",
+  openGraph: {
+    images: '/images/GeekPieOGImage.jpg'
+  }
 };
 
 export default function RootLayout({
@@ -23,12 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      {/* <head>
         <meta property="og:image" content="/images/GeekPieOGImage.jpg" />
         <meta property="og:image:type" content="image/jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-      </head>
+      </head> */}
       <body className={`${GeistSans.className} bg-[#070815] text-white`}>
         <Header />
         <main className="w-full">
