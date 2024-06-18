d24502aad5cabbacccf4bd1e9850b3c7146dfd5b
Tokom Nyori <tokom.nyori@gmail.com>
2024-06-18 06:43:57 +0530

    just edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         heading="Chatbots that radiate with dynamic intelligence."
         paragraph="AI-Powered Chatbots: Disrupt and Capture the Market, Propelling Your Business Forward."
         buttonText="Try Now!"
-        imageSrc="/images/hero-image.png"
-        imageSrcMobile="/images/hero-image-sm.png"
+        imageSrc="/images/geekpieaihero.jpg"
+        imageSrcMobile="/images/trialsm.jpg"
         alt="Hero Image"
       />
       <Features />
