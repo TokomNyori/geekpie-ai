ff6653ca557f2bf284a5680c6ff7c3d14fb60b61
Tokom Nyori <tokom.nyori@gmail.com>
2024-06-25 23:11:30 +0530

    just edit 4

--- src/components/major/AIChatBox.tsx
+++ src/components/major/AIChatBox.tsx
@@ -36,11 +36,11 @@ const AIChatBox = ({ open, onClose }: AIChatBoxProps) => {
     }
   }, [messages]);
 
-  useEffect(() => {
-    if (open) {
-      inputRef.current?.focus();
-    }
-  }, [open]);
+  // useEffect(() => {
+  //   if (open) {
+  //     inputRef.current?.focus();
+  //   }
+  // }, [open]);
 
   const isLastMessageByUser = messages[messages.length - 1]?.role === "user";
 
@@ -57,7 +57,7 @@ const AIChatBox = ({ open, onClose }: AIChatBoxProps) => {
       >
         <IoCloseCircleOutline />
       </button>
-      <div className="relative flex h-[80vh] flex-col rounded-lg border border-gray-500/50 bg-black/60 backdrop-blur-[12px] md:h-[37.5rem]">
+      <div className="relative flex h-[80vh] flex-col rounded-2xl border border-gray-500/50 bg-black/60 backdrop-blur-[12px] md:h-[37.5rem]">
         <div className="absolute inset-0 top-16 -z-10 aspect-square rounded-full bg-blue-400/25 blur-3xl filter"></div>
         <div
           className="mt-4 h-full w-full overflow-y-auto px-4"
