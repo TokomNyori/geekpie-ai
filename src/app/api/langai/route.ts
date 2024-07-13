7d22758de22d0efa7d693e2fa86fb1137af8468f
Tokom Nyori <tokom.nyori@gmail.com>
2024-07-14 03:38:12 +0530

    geekpie ai bot - 11

--- src/app/api/langai/route.ts
+++ src/app/api/langai/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: NextRequest) {
         })
 
         const rephrasingModel = new ChatGoogleGenerativeAI({
-            model: "gemini-1.5-flash",
+            model: "gemini-1.5-pro",
             verbose: true,
         })
 
